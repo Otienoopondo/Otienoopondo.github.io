368aad8b364277f8b55403e8574c460db6600ade
Otienoopondo <119538155+Otienoopondo@users.noreply.github.com>
2024-02-13 14:03:23 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+
+console.log(4+8)
+console.log('sam the greattest')
+let firstname='samwel'
+let secondname='opondo'
+let city='nairobi'
+console.log(firstname,secondname,city)
+console.log(Math.round(9.87))
+let number=(Math.random()*100)
+console.log(Math.ceil(number))
+
